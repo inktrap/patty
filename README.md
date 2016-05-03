4f38c37a31c7102bb5c0a0fb31e5f5799c2cc733
inktrap <inktrap@inktrap.org>
2016-05-03 20:56:48 +0200

    class instead of function and options as parameters

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # Bugs
 
- - always handing over the settings via a parameter -- there has to be a better way … ask on SO (this is not really a bug)
+ - :tada:
 
 
 # ToDo
 
- - styling
- - test with safari and mozilla
- - maybe write profiles, like trump, sanders, cyber, cloud, … (load them via json?)
  - be proud & happy!!! first addon :) yay <3 :tada: :clap: :smile:
-
+ - styling?
+ - test with safari and opera?
+ - maybe write profiles, like trump, sanders, cyber, cloud, … (load them from json?)
+ - maybe hide the addon button so it's more pranky?
 
 # Done
 
  - x the dom tree gets traversed multiple times, this makes the order of rules unimportant
  - x regexes should be compiled only once
+ - x always handing over the settings via a parameter -- there has to be a better way … ask on SO (this is not really a bug) (thought about it for a minute and used a class instead of functions …
 
 # Prerequisites
 
--- content.js
+++ content.js
@@ -1,64 +1,72 @@
 /*jshint esversion: 6 */
 
-// <https://stackoverflow.com/questions/5904914/javascript-regex-to-replace-text-not-in-html-attributes/5904945#5904945>
-function walk(rules, node)
-{
-    // Source: http://is.gd/mwZp7E
+function Handler(rules){
+    this.rules = rules;
+    // <https://stackoverflow.com/questions/5904914/javascript-regex-to-replace-text-not-in-html-attributes/5904945#5904945>
 
-    var child, next;
-
-    switch ( node.nodeType )
+    this.walk = function(node)
     {
-        case 1:  // Element
-        case 9:  // Document
-        case 11: // Document fragment
-            child = node.firstChild;
-            while ( child )
-            {
-                next = child.nextSibling;
-                walk(rules, child);
-                child = next;
-            }
-            break;
+        // Source: http://is.gd/mwZp7E
 
-        case 3: // Text node
-            handleNode(rules, node);
-            break;
-    }
-}
+        var child, next;
 
-function handleNode(rules, textNode) {
-    let v = textNode.nodeValue;
+        switch ( node.nodeType )
+        {
+            case 1:  // Element
+            case 9:  // Document
+            case 11: // Document fragment
+                child = node.firstChild;
+                while ( child )
+                {
+                    next = child.nextSibling;
+                    this.walk(child);
+                    child = next;
+                }
+                break;
 
-    if (v) {
-        let modified =  handleText(rules, v);
-
-        if (v != modified) {
-            textNode.nodeValue = modified;
+            case 3: // Text node
+                this.handleNode(node);
+                break;
         }
-    }
+    };
+
+    this.handleNode = function(textNode) {
+        let v = textNode.nodeValue;
+
+        if (v) {
+            let modified =  this.handleText(v);
+
+            if (v != modified) {
+                textNode.nodeValue = modified;
+            }
+        }
+    };
+
+    this.handleText = function(text) {
+        let v = text;
+
+        for (var i=0; i<this.rules.length; i++){
+            v = v.replace(this.rules[i][0], this.rules[i][1]);
+        }
+
+        return v;
+    };
+
+    this.handleTitle = function() {
+        document.title = this.handleText(document.title);
+    };
 }
 
-function handleText(rules, text) {
-    let v = text;
-
-    for (var i=0; i<rules.length; i++){
-        v = v.replace(new RegExp(rules[i][0], 'g'), rules[i][1]);
-    }
-
-    return v;
-}
-
-function handleTitle(rules) {
-    document.title = handleText(rules, document.title);
-}
 
 function main(rules){
     //console.log(rules);
     if ((rules  !== undefined) && (rules !== null)){
+        var handler = new Handler(rules);
+        handler.walk(document.body);
+        handler.handleTitle();
         //setTimeout(function () {
-        walk(rules, document.body);
-        handleTitle(rules);
+        //walk(rules, document.body);
+        //handleTitle(rules);
         //}, 10);
     }
 }
